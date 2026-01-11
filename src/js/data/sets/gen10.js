a15adea0947f101b5115b62bc9f5c7959a7aa769
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-01-12 00:55:08 +0100

    HATE. LET ME TELL YOU HOW MUCH I HAVE COME TO HATE

--- src/js/data/sets/gen10.js
+++ src/js/data/sets/gen10.js
@@ -1,5 +1,5 @@
 var SETDEX_NDC = {
-	"Haxorus": {
+	 'Haxorus': {
 		"1v1 Choice Band": {"ability":"Unnerve","item":"Choice Band","nature":"Adamant","evs":{"at":252,"df":4,"sp":252},"moves":["Outrage","Scale Shot","First Impression","Close Combat"]}
 	},
 };
