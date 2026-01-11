f61201ca35234b12938a524ff465a2e78db09685
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-01-12 00:34:42 +0100

    Update gen10.js

--- src/js/data/sets/gen10.js
+++ src/js/data/sets/gen10.js
@@ -1,6 +1,6 @@
 var SETDEX_NDC = {
-	"Ivysaur": {
-		"NFE Defensive": {"ability":"Overgrow","item":"Eviolite","nature":"Bold","evs":{"hp":252,"df":252,"sd":4},"moves":["Knock Off","Sludge Bomb","Giga Drain","Synthesis"]}
+	"Haxorus": {
+		"1v1 Choice Band": {"ability":"Unnerve","item":"Choice Band","nature":"Adamant","evs":{"at":252,"df":4,"sp":252},"moves":["Outrage","Scale Shot","First Impression","Close Combat"]}
 	},
 };
 // Mandatory at least one set or else the app throws a hissy fit
