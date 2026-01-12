e06d618ad1e17b4f4e70373f8e4dcb98c20f20f5
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-01-12 01:30:20 +0100

    Update gen10.js

--- src/js/data/sets/gen10.js
+++ src/js/data/sets/gen10.js
@@ -1,7 +1,7 @@
 var SETDEX_NDC = {
 	 "Haxorus": {
 		 "BSS Reg J Mold Breaker Funny Moments Compilation": {"level":50,"ability":"Mold Breaker","item":"Life Orb","nature":"Jolly","teraType":"Steel","evs":{"at":252,"sd":4,"sp":252},"moves":["Dragon Dance","Dragon Claw","Earthquake","Iron Head"]},
-		"1v1 Choice Band": {"ability":"Unnerve","item":"Choice Band","nature":"Adamant","evs":{"at":252,"df":4,"sp":252},"moves":["Outrage","Scale Shot","First Impression","Close Combat"]}
+		"1v1 Choice Band": {"ability":"Unnerve","item":"Choice Band","nature":"Adamant","evs":{"at":252,"df":4,"sp":252},"moves":["Outrage","Scale Shot","First Impression","Close Combat"]},
 	 },
 };
 // Mandatory at least one set or else the app throws a hissy fit
