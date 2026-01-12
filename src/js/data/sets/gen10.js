a77333e7beb5d988f28dd435e9ab80b0cdf25d0f
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-01-12 01:06:36 +0100

    Update gen10.js

--- src/js/data/sets/gen10.js
+++ src/js/data/sets/gen10.js
@@ -1,6 +1,6 @@
 var SETDEX_NDC = {
-	 'Haxorus': {
+	 "Haxorus": {
 		"1v1 Choice Band": {"ability":"Unnerve","item":"Choice Band","nature":"Adamant","evs":{"at":252,"df":4,"sp":252},"moves":["Outrage","Scale Shot","First Impression","Close Combat"]}
-	},
+	 },
 };
 // Mandatory at least one set or else the app throws a hissy fit
