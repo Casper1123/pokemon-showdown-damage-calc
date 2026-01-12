d1987fc8c7c0b4106ff1e22314fea4da405d37da
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-01-12 01:28:13 +0100

    Update gen10.js

--- src/js/data/sets/gen10.js
+++ src/js/data/sets/gen10.js
@@ -1,5 +1,6 @@
 var SETDEX_NDC = {
 	 "Haxorus": {
+		 "BSS Reg J Mold Breaker Funny Moments Compilation": {"level":50,"ability":"Mold Breaker","item":"Life Orb","nature":"Jolly","teraType":"Steel","evs":{"at":252,"sd":4,"sp":252},"moves":["Dragon Dance","Dragon Claw","Earthquake","Iron Head"]},
 		"1v1 Choice Band": {"ability":"Unnerve","item":"Choice Band","nature":"Adamant","evs":{"at":252,"df":4,"sp":252},"moves":["Outrage","Scale Shot","First Impression","Close Combat"]}
 	 },
 };
